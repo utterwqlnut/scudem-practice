9728621db73552834722165ad24db5bfbbbe1524
Dhruva <dhruva.chayapathy@gmail.com>
2024-10-07 00:49:50 -0400

    quick cleanup

--- projectile.py
+++ projectile.py
@@ -11,11 +11,21 @@ class Projectile:
         self.v_x0 = v_x0
         self.G = 10
     
+    def get_x(t):
+        x=self.x_0-((self.m/self.b)*self.v_x0*(1-np.e**(-self.b*t/self.m)))
+        return x
+
+    def get_y(t):
+        y=self.y_0-((self.m*self.G/self.b)*t+(self.m/self.b)*(self.v_y0-self.m*self.G/self.b)*(1-np.e**(-self.b*t/self.m))))
+        return y
+
     def simulate_points(self, N, T):
         x = []
         y = []
+
+        # 100 evenly spaced points from time 0 to time T
         for t in np.linspace(0,T,N):
-            x.append(self.x_0-((self.m/self.b)*self.v_x0*(1-np.e**(-self.b*t/self.m))))
-            y.append(self.y_0-((self.m*self.G/self.b)*t+(self.m/self.b)*(self.v_y0-self.m*self.G/self.b)*(1-np.e**(-self.b*t/self.m))))
+            x.append(get_x(t))
+            y.append(get_y(t))
         
         return (x,y)
